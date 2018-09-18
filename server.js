a8f0acd5c8acf96f891b93aa2012a4d30370444d
Silas Owings <silasowings@gmail.com>
2018-09-18 15:39:25 -0500

    refactor w hbs partials
    improved page templates with handlebars

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('upCase', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   // replace with render of home page
   res.render('home.hbs',{
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMsg: 'Welcome to my Express.JS site'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
