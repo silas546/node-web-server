1f3968f6b7791d73744060f4607c2e13e5f1ece1
Silas Owings <silasowings@gmail.com>
2018-10-04 12:47:41 -0500

    project views init commit

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'You done messt up'
